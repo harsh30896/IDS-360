d1673281e6037d81c46c562969edc328090737d0
Harsh <harsh@Harshs-MacBook-Pro.local>
2026-04-02 22:17:56 +0530

    security config

--- src/main/java/com/ids360/config/SecurityConfig.java
+++ src/main/java/com/ids360/config/SecurityConfig.java
@@ -18,7 +18,7 @@ public class SecurityConfig {
         http
             .csrf(AbstractHttpConfigurer::disable)
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/api/v1/auth/**").permitAll()
+                    .requestMatchers("/api/v1/auth/**", "/api/v1/createOrganization").permitAll()
                 .anyRequest().authenticated()
             );
 
